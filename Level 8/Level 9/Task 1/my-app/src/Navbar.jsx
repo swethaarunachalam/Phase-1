9b577956ed2f75714f4e92e634bc59ff1e4a8a8f
swethaarunachalam <swethaarunachalam21@gmail.com>
2025-03-24 09:20:44 +0530

    Add files via upload

---
+++ Level 8/Level 9/Task 1/my-app/src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './App.css';
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
