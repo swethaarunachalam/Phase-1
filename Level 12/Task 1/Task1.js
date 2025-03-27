162ccd97cb24dc1777aefffeea1ee09469a06cda
swethaarunachalam <swethaarunachalam21@gmail.com>
2025-03-27 09:12:12 +0530

    Delete Level 12 directory

--- Level 12/Task 1/Task1.js
+++
@@ -1,8 +0,0 @@
-const fs = require ('fs');
-
-fs.readFile('sample.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file:', err); 
-    }
-    console.log('file contents:\n',data);
-});
